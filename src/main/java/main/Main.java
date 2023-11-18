4ce98cf50070822ba565b312ce32e096752c6ba8
ort <orthodoxplus@ya.ru>
2023-11-18 21:15:37 +0300

    add git and dependencies

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -3,8 +3,11 @@ package main;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.time.ZoneId;
+
 @SpringBootApplication
 public class Main {
+	public static final ZoneId zone = ZoneId.of("Europe/Moscow");
 	public static void main(String[] args) {
 		SpringApplication.run(Main.class, args);
 	}

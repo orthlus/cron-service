c46cb6a26e2370cf4ace85fc9db7edb6838cfc0a
ort <orthodoxplus@ya.ru>
2024-07-16 10:30:53 +0300

    s3

--- src/main/java/main/cloudflare/CloudflareBackuper.java
+++ src/main/java/main/cloudflare/CloudflareBackuper.java
@@ -1,16 +1,10 @@
 package main.cloudflare;
 
 import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.model.ObjectMetadata;
-import com.amazonaws.services.s3.transfer.TransferManager;
-import com.amazonaws.services.s3.transfer.TransferManagerBuilder;
-import com.amazonaws.services.s3.transfer.Upload;
 import lombok.extern.slf4j.Slf4j;
 import main.rest.S3Client;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-
-import java.io.ByteArrayInputStream;
 
 @Slf4j
 @Component
@@ -28,18 +22,7 @@ public class CloudflareBackuper extends S3Client {
 
 	public void uploadFileContent(String fileId, String fileContent) {
 		try {
-			AmazonS3 s3 = client();
-			TransferManager tm = TransferManagerBuilder.standard()
-					.withS3Client(s3)
-					.build();
-			byte[] bytes = fileContent.getBytes();
-			ByteArrayInputStream fileData = new ByteArrayInputStream(bytes);
-			ObjectMetadata objectMetadata = new ObjectMetadata();
-			objectMetadata.setContentLength(bytes.length);
-			Upload upload = tm.upload(bucket, fileId, fileData, objectMetadata);
-			upload.waitForCompletion();
-			tm.shutdownNow();
-			s3.shutdown();
+			uploadFileContent(fileId, fileContent, client(), bucket);
 		} catch (InterruptedException e) {
 			log.error("s3 error - upload file {}/{}", bucket, fileId, e);
 			throw new RuntimeException(e);

31f3f5f195c878e283f6acbd54a07e67ea3346b0
ort <orthodoxplus@ya.ru>
2024-07-16 12:25:11 +0300

    telegram backup

--- src/main/java/main/telegram/backup/TelegramBackuperService.java
+++ src/main/java/main/telegram/backup/TelegramBackuperService.java
@@ -1,12 +1,14 @@
 package main.telegram.backup;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import static main.Utils.now;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class TelegramBackuperService {
@@ -18,8 +20,9 @@ public class TelegramBackuperService {
 	@Scheduled(cron = "${cron.telegram.backup}", zone = "Europe/Moscow")
 	public void backup() {
 		String chatBackupContent = telegramBackuperClient.getChatBackupContent();
-		String fileName = "%s-%s.json".formatted(s3Filename, now());
+		String filename = "%s-%s.json".formatted(s3Filename, now());
 
-		telegramBackuperS3.uploadFileContent(fileName, chatBackupContent);
+		telegramBackuperS3.uploadFileContent(filename, chatBackupContent);
+		log.info("telegram backup {} complete", filename);
 	}
 }
